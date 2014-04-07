e05e04d6fa47d603a5e614119d4496a8b94486eb
Joseph Nudell <joenudell@gmail.com>
2014-04-07 01:24:53 -0500

    add fields for api

---
+++ api/wood_fields.py
@@ -0,0 +1,55 @@
+#-*-coding:utf8-*-
+'''
+Fields in the table
+
+Copyright 2014 Joe Nudell
+'''
+
+db_name = 'wood'
+
+LIST_DELIM = '|'
+
+fields = {
+    'common_name' : 'varchar[]',
+    'scientific_name' : 'varchar[]',
+    'distribution' : 'varchar',
+    'tree_height_imperial' : 'float8[]',
+    'tree_height_metric' : 'float8[]',
+    'tree_diameter_imperial' : 'float8[]',
+    'tree_diameter_metric' : 'float8[]',
+    'average_dried_weight_imperial' : 'float8',
+    'average_dried_weight_metric' : 'float8',
+    'specific_gravity_basic' : 'float8',
+    'specific_gravity_12pct' : 'float8',
+    'janka_hardness_imperial' : 'float8',
+    'janka_hardness_metric' : 'float8',
+    'modulus_of_rupture_imperial' : 'float8',
+    'modulus_of_rupture_metric' : 'float8',
+    'elastic_modulus_imperial' : 'float8',
+    'elastic_modulus_metric' : 'float8',
+    'crushing_strength_imperial' : 'float8',
+    'crushing_strength_metric' : 'float8',
+    'shrinkage_radial' : 'float8',
+    'shrinkage_tangential' : 'float8',
+    'shrinkage_volumetric' : 'float8',
+    'shrinkage_tr_ratio' : 'float8',
+
+    'wood_type' : 'varchar',
+
+    'text_color' : 'varchar',
+    'text_grain' : 'varchar',
+    'text_endgrain' : 'varchar',
+    'text_rot' : 'varchar',
+    'text_workability' : 'varchar',
+    'text_odor' : 'varchar',
+    'text_toxicity' : 'varchar',
+    'text_price' : 'varchar',
+    'text_sustainability' : 'varchar',
+    'text_uses' : 'varchar',
+    'text_comments' : 'varchar',
+    'text_related' : 'varchar',
+
+    'url_pics' : 'varchar',
+
+    'url' : 'varchar'
+}
--- script/reset_db.py
+++ script/reset_db.py
@@ -9,20 +9,24 @@ Copyright 2014 Joe Nudell
 
 import json
 import psycopg2 as pg
-from sys import argv, stderr, exit
+import os
+import sys
 
-db_name = 'wood'
-fields = {
-    'common_name' : 'varchar',
-    'scientific_name': 'varchar'
-}
-LIST_DELIM = '|'
+# put API module in path
+real_path = os.path.dirname(__file__)
+api_rel = '../api'
+api_full_path = os.path.join(real_path, api_rel)
+sys.path.append(api_full_path)
+
+from wood_fields import db_name, fields, LIST_DELIM
+
+
 
 # SQL for creating the table
 sql_create = '''
 CREATE TABLE %s
 (
-    id serial PRIMARY_KEY,
+    id serial PRIMARY KEY,
     %s
 );
 ''' % (
@@ -37,41 +41,72 @@ sql_drop = '''
 DROP TABLE IF EXISTS %s
 ''' % db_name
 
+sql_insert = '''
+INSERT INTO %s (%%s)
+VALUES (%%s)
+''' % db_name
+
 def drop(cur):
     cur.execute(sql_drop)
 
 def create(cur):
     cur.execute(sql_create)
 
+def sanitize(v, esc=True):
+    if type(v) is list:
+        v = json.dumps([sanitize(a, False) for a in v])
+        v = v.replace('[', '{').replace(']', '}')
+        v = "'%s'" % v
+    elif type(v) is str:
+        v = "'%s'" % v if esc else v
+    elif type(v) is unicode:
+        v = u'"' + v + '"' if esc else v
+    elif v is None:
+        v = 'null'
+    else:
+        v = unicode(v)
+    return v
 
-# def clean(k, v):
-#     if type(v) is list:
-#         v = LIST_DELIM.join(v)
-#     return '%s %s' % (k, v)
+def insert(cur, d):
+    keys = []
+    values = ''
+    for k, v in d.items():
+        keys.append(k)
+        values += ',' if len(values) else ''
+        values += sanitize(v)
+    sql = sql_insert % (
+        ','.join(keys),
+        values
+    )
+    cur.execute(sql)
 
 
 if __name__ == '__main__':
-    if len(argv) != 3:
-        print >>stderr, __doc__
-        exit(1)
+    if len(sys.argv) != 3:
+        print >>sys.stderr, __doc__
+        sys.exit(1)
 
-    print >>stderr, "Reading in records ..."
-    with open(argv[2]) as fh:
+    print >>sys.stderr, "Reading in records ..."
+    with open(sys.argv[2]) as fh:
         text = fh.read()
         records = json.loads(text)
 
     # connect to postgres
-    print >>stderr, "Connecting to database ..."
-    conn = pg.connect("dbname=%s user=%s" % (argv[1], argv[1]))
+    print >>sys.stderr, "Connecting to database ..."
+    conn = pg.connect("dbname=%s user=%s" % (sys.argv[1], sys.argv[1]))
     cur = conn.cursor()
 
-    print >>stderr, "Destroying any old tables ..."
+    print >>sys.stderr, "Destroying any old tables ..."
     drop(cur)
-    print >>stderr, "Creating new tables ..."
+    print >>sys.stderr, "Creating new tables ..."
     create(cur)
 
+    print >>sys.stderr, "Adding records to database ..."
+    for record in records:
+        insert(cur, record)
+
     # persist & close
-    print >>stderr, "Closing connections"
+    print >>sys.stderr, "Closing connections"
     conn.commit()
     cur.close()
     conn.close()
