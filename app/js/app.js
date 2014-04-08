661566f5a5f307a46a4f73ce8bf544c74efaa6fe
Joseph Nudell <joenudell@gmail.com>
2014-04-08 09:16:15 -0500

    add js dependencies

---
+++ app/js/app.js
@@ -0,0 +1,16 @@
+/**
+ * Main app entry point
+ *
+ * Copyright 2014 Joe Nudell
+ */
+
+define(function() {
+
+    var App = {
+        start: function() {
+            console.log("App started")
+        }
+    };
+
+    return App;
+});
---
+++ app/js/main.js
@@ -0,0 +1,51 @@
+/**
+ * RequireJS bootstrapper
+ *
+ * Copyright 2014 Joe Nudell. All rights reserved.
+ */
+
+require.config({
+
+    baseUrl: '/js',
+
+    paths: {
+        text        : 'lib/require-text',
+        zepto       : 'lib/zepto',
+        underscore  : 'lib/underscore',
+        d3          : 'lib/d3',
+        crossfilter : 'lib/crossfilter'
+    },
+
+    shim: {
+        d3: {
+            exports: 'd3'
+        },
+        zepto: {
+            exports: 'Zepto'
+        },
+        underscore: {
+            exports: '_'
+        },
+        crossfilter : {
+            exports: 'crossfilter'
+        }
+    }
+
+});
+
+
+// start app
+require([
+    'zepto',
+    'app'
+],
+function(
+    Zepto,
+    app
+) {
+
+    $(function() {
+        app.start();
+    });
+
+});
